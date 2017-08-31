2246507ec02e8ba85c800b2973a14e0c20cfdc34
wangyipeng <wangyipeng@autohome.com.cn>
2017-08-31 20:23:20 +0800

    修改颜色

--- WF/WF/Content/js/wfdesign.js
+++ WF/WF/Content/js/wfdesign.js
@@ -104,3 +104,17 @@ function wfnode(options) {
     this.nodeText.attr({ "font-size": this.settings.fontSize });
     this.nodeText.attr({ "title": this.settings.text });
 };
+
+function getArr(x1, y1, x2, y2, size) {
+    var angle = Raphael.angle(x1, y1, x2, y2);
+    var a45 = Raphael.rad(angle - 45);
+    var a45m = Raphael.rad(angle + 45);
+    var x2a = x2 + Math.cos(a45) * size;
+    var y2a = y2 + Math.sin(a45) * size;
+    var x2b = x2 + Math.cos(a45m) * size;
+    var y2b = y2 + Math.sin(a45m) * size;
+    var result = ["M", x1, y1, "L", x2, y2, "L", x2a, y2a, "M", x2, y2, "L", x2b, y2b];
+    return result;
+}
+var pathth = this.path(getArr(1,1,200,200,10));
+pathth.attr({ "stroke-width": 4, "stroke": "#5DA95E" });
